a1eb00e4fb387bc69577561a62ff5630a0ddc9b3
Hristo Hristov <hristo.hristov@telerik.com>
2017-08-07 17:24:12 +0300

    cache page on forward navigation (#4652)
    * cache page on forward navigation
    Still some failing navigation tests
    
    * Current page is kept alive when navigating forward
    Refactoring code and removing all hacks and flags
    Remove one module circular reference
    
    * Disable Page recycling because when there is transition between pages the nativeView stays animated (e.g. when transition is Fade the hidden page nativeView stays with Alpha 0)
    Disable recycling if there is native anitmation
    
    * Fix failing tests on ios & android API17
    Fix wrong urls in http tests
    Made some timer tests async
    
    * Animations are not stored in BackstackEntry instead of Fragment because fragments could die (activity die) and recreated and we lose animations.
    
    * Fix android crash when activity is recreated.
    Refactoring transitionListener.

--- tns-platform-declarations/android/android17.d.ts
+++ tns-platform-declarations/android/android17.d.ts
@@ -213068,6 +213068,16 @@ declare module android {
 			restoreViewState(savedInstanceState: android.os.Bundle): void;
 			setIndex(index: number, parent: android.app.Fragment): void;
 			isInBackStack(): boolean;
+			getEnterTransition(): android.transition.Transition;
+			getExitTransition(): android.transition.Transition;
+			getReenterTransition(): android.transition.Transition;
+			getReturnTransition(): android.transition.Transition;
+			setEnterTransition(transition: android.transition.Transition): void;
+			setExitTransition(transition: android.transition.Transition): void;
+			setReenterTransition(transition: android.transition.Transition): void;
+			setReturnTransition(transition: android.transition.Transition): void;
+			setAllowEnterTransitionOverlap(allow: boolean): void;
+			setAllowReturnTransitionOverlap(allow: boolean): void;
 			/**
 			 * Subclasses can not override equals().
 			 */
@@ -234451,6 +234461,44 @@ declare module android {
 			public handleChunk(request: org.apache.harmony.dalvik.ddmc.Chunk): org.apache.harmony.dalvik.ddmc.Chunk;
 		}
 	}
+
+	export module transition {
+		export abstract class Transition extends java.lang.Object {
+			addListener(transition: Transition.TransitionListener): Transition;
+			removeListener(transition: Transition.TransitionListener): Transition;
+			setDuration(duration: number): Transition;
+			setInterpolator(interpolator: android.animation.TimeInterpolator): Transition;
+		}
+
+		export abstract class Visibility extends android.transition.Transition {
+			constructor();
+		}
+
+		export class Slide extends Visibility {
+			constructor(slideEdge: number);
+		}
+
+		export class Fade extends Visibility {
+			constructor(fadingMode: number);
+			static IN: number;
+			static OUT: number;
+		}
+
+		export class Explode extends Visibility {
+			constructor();
+		}
+
+		export module Transition {
+			export interface TransitionListener {
+				onTransitionStart(transition: android.transition.Transition): void;
+				onTransitionEnd(transition: android.transition.Transition): void;
+				onTransitionResume(transition: android.transition.Transition): void;
+				onTransitionPause(transition: android.transition.Transition): void;
+				onTransitionCancel(transition: android.transition.Transition): void;
+			}
+		}
+	}
+
 	export module animation {
 		/**
 		 * Listener tests for ObjectAnimator.
